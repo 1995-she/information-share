9c1d62d728fb5b45e5c758ea6d926d119d375888
1995-she <2265627598@qq.com>
2025-05-06 16:39:09 +0800

    显示登录信息

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+
+
+
+
+}
